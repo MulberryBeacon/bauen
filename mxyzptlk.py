8cb8cac68a17a5e7249b945f7935c6ce09490018
Eduardo Ferreira <mulberry.beacon@gmail.com>
2019-04-01 19:39:51 +0100

    First working version of the repository cloning script.

--- mxyzptlk.py
+++ mxyzptlk.py
@@ -4,31 +4,66 @@
 Script to automatically configure a workspace with a list of GitHub repositories.
 """
 
-import requests
-from gitpython import Repo
+from pathlib import Path
 
-def get_repo_list(username: str, token: str) -> list:
+import requests
+from git import Repo
+
+
+_BASE_DIRECTORY = 'work2/github'
+_HOME = str(Path.home().resolve())
+
+
+def get_repo_list(token: str) -> list:
     """
     Retrieves the list of repositories for the given username.
+
+    :param token:
+        The GitHub Personal Access Token
+
+    :returns:
+        A JSON object with the list of repositories
     """
-    url = 'https://api.github.com/users/{}/repos'.format(username) 
+    url = 'https://api.github.com/user/repos'
     headers = { 'Authorization': 'token {}'.format(token) }
     r = requests.get(url, headers=headers)
-    return r.json()
 
-def clone_repo(repo_ssh: str, destination: str):
-    """
-    """
-    Repo.clone_from(repo_ssh, destination)
+    if r.status_code == requests.codes.ok:
+        return r.json()
+    else:
+        r.raise_for_status()
 
-def action():
+
+def clone_repo(repo_ssh: str) -> str:
     """
+    Clones a repository.
+
+    :param repo_ssh:
+        The SSH repository address
+
+    :returns:
+        True if the repository was cloned successfully; False otherwise
     """
-    response = get_repo_list('MulberryBeacon', '2f2edfd3564ed6832fc862267a3ced3aca80eb91')
+    path = str(Path(Path.home(), _BASE_DIRECTORY, Path(repo_ssh).stem).resolve())
+    repo = Repo.clone_from(repo_ssh, path)
+    if repo and repo.working_tree_dir == path:
+        return repo.working_tree_dir
+    else:
+        return None
+
+
+def clone_repos():
+    """
+    Fetches the list of repositories and clones them into the desired directory.
+    """
+    response = get_repo_list('2f2edfd3564ed6832fc862267a3ced3aca80eb91')
     for x in response:
-        ssh_url = x['ssh_url']
-        clone_repo(ssh_url, '.')
+        repo = clone_repo(x['ssh_url'])
+        if repo:
+            print('Repository "{}" created successfully'.format(repo))
+        else:
+            print('Couldn\' clone repository {}'.format(repo))
 
 
 if __name__ == '__main__':
-    action()
+    clone_repos()
